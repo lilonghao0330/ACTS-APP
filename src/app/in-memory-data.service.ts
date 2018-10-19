6c3cf25b7e6321926c21e3977beb7872f8cf16f7
lilonghao <longhao.li@accenture.com>
2018-10-19 11:44:01 +0900

    テーブルたたき作成

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,16 +4,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 1, name: '田中' },
-      { id: 2, name: '加藤' },
-      { id: 3, name: '伊藤' },
-      { id: 4, name: '佐野' },
-      { id: 5, name: '太田' },
-      { id: 6, name: '山本' },
-      { id: 7, name: '中村' },
-      { id: 8, name: '小林' },
-      { id: 9, name: '加藤' },
-      { id: 10, name: '中村' }
+      { id: 1, name: '田中',order:17,goal:15},
+      { id: 2, name: '加藤',order:16,goal:15},
+      { id: 3, name: '伊藤',order:14,goal:15},
+      { id: 4, name: '佐野',order:12,goal:15},
+      { id: 5, name: '太田',order:11,goal:10},
+      { id: 6, name: '山本',order:6,goal:10},
+      { id: 7, name: '中村',order:5,goal:10},
+      { id: 8, name: '小林',order:3,goal:5},
+      { id: 9, name: '加藤',order:2,goal:5},
+      { id: 10, name: '中村',order:1,goal:5}
     ];
     return {heroes};
   }
